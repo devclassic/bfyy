00f9aa21dc0f047fccbdfe4492d4faa9c74a0490
devclassic <devclassic@yeah.net>
2025-05-28 16:20:08 +0800

    修改打包命令

--- server/tasks.py
+++ server/tasks.py
@@ -3,4 +3,4 @@ from invoke import task
 
 @task
 def build(c):
-    c.run("pyinstaller -F --collect-all tortoise main.py")
+    c.run("pyinstaller -F --collect-all tortoise --hidden-import main main.py")
