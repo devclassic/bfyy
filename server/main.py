5c8bb1f2fc3d65f1fdeae799d4afed34b551229e
devclassic <devclassic@yeah.net>
2025-05-28 16:05:37 +0800

    更改负端启动为多进程

--- server/main.py
+++ server/main.py
@@ -72,4 +72,4 @@ if __name__ == "__main__":
     import uvicorn
 
     multiprocessing.freeze_support()
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=4)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=4)
